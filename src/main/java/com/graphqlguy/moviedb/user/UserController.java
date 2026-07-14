351bda6259bc397e89e1f935bc6e69854c4b922e
zkozina <prof.zeljko@gmail.com>
2026-07-14 15:05:31 +0100

    Class 4 fixes: consistent constructor injection in UserController, drop unused imports, remove premature RegisterInput description

--- src/main/java/com/graphqlguy/moviedb/user/UserController.java
+++ src/main/java/com/graphqlguy/moviedb/user/UserController.java
@@ -1,6 +1,6 @@
 package com.graphqlguy.moviedb.user;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Controller;
 import java.security.Principal;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @MutationMapping
     AuthResponse login(@Argument LoginInput input) {
